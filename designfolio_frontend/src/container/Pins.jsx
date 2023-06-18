84c69d367cac8c2c2289a65dcc30c6f72ed707fc
Riya <triya1315@gmail.com>
2023-06-18 22:55:25 +0530

    Components

--- designfolio_frontend/src/container/Pins.jsx
+++ designfolio_frontend/src/container/Pins.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { Route,Routes } from 'react-router-dom'
+import { NavBar,Feed,CreatePin,Search,PinDetail } from '../components'
 
 const Pins = () => {
   return (
