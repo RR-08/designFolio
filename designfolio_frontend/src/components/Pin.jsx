2af038cd00326979040b8b8b27d1f07c9bf3301a
Riya <triya1315@gmail.com>
2023-08-16 01:42:35 +0530

    Save Pin

--- designfolio_frontend/src/components/Pin.jsx
+++ designfolio_frontend/src/components/Pin.jsx
@@ -5,19 +5,25 @@ import {v4 as uuidv4} from 'uuid'
 import {MdDownloadForOffline} from 'react-icons/md'
 import { AiTwotoneDelete } from "react-icons/md";
 import { BsFillArrrowUpRightCircleFill} from "react-icons/md";
+import { fetchUser } from '../utils/fetchUser'
 
 
 
 
 
-const Pin = ({pin:{postedBy,image,_id,destination}}) => {
-  
-  const[postHovered,setPostHovered]=useState(false);
+const Pin = ({pin:{postedBy,image,_id,destination,save}}) => {
+  const [postHovered, setPostHovered] = useState(false);
   const [savingPost, setSavingPost] = useState(false);
-  const navigate=useNavigate();
+  const navigate = useNavigate();
 
+  const user = fetchUser();
+  const alreadySaved = !!(save?.filter(
+    (item) => item.postedBy._id === user.googleId
+  ))?.length;
 
-
+  //1,[2,3,1]->[1].length->1 ->!1->false->!false->true
+  //4,[2,3,1]->[].length->0 ->!0->true->!true->false
+  
 
   return (
     <div className="m-2 ">
@@ -32,22 +38,44 @@ const Pin = ({pin:{postedBy,image,_id,destination}}) => {
           alt="user-post"
           src={urlFor(image).width(250).url()}
         />
-        {postHovered &&(
+        {postHovered && (
           <div
-          className='absolute top-0 w-full h-full flex flex-col justify-between p-1 pr-2 pt-2 pb-2 z-50'
-          style={{height:'100%'}}
+            className="absolute top-0 w-full h-full flex flex-col justify-between p-1 pr-2 pt-2 pb-2 z-50"
+            style={{ height: "100%" }}
           >
-            <div className='flex items-center justify-between'>
-              <div className='flex'>
-                <a href={`${image?.asset?.url}?dl=`}
-                download
-                onClick={(e)=>e.stopPropagation()}
-                className='bg-white w-9 h-9 rounded-full flex items-center justify-center text-dark text-al opacity-75 hover:opacity-100 absolute'
-                />
-                <MdDownloadForOffline/>
+            <div className="flex items-center justify-between">
+              <div className="flex gap-2">
+                <a
+                  href={`${image?.asset?.url}?dl=`}
+                  download
+                  onClick={(e) => e.stopPropagation()}
+                  // stop it from going to pin image page
+                  className="bg-white w-9 h-9 rounded-full flex items-center justify-center text-dark text-xl opacity-75 hover:opacity-100 absolute"
+                >
+                  <MdDownloadForOffline />
+                </a>
               </div>
+              {alreadySaved ? (
+                <button
+                  type="button"
+                  className="bg-red-500 opacity-70 hover:opacity-100 text-white font-bold px-5 py-1 text-base rounded-3xl hover:shadow-md outlined-none"
+                >
+                  Saved
+                </button>
+              ) : (
+                <button
+                onClick={(e)=>{
+                  e.stopPropagation();
+                savePin(_id);
+                }
+                  }
+                  type="button"
+                  className="bg-red-500 opacity-70 hover:opacity-100 text-white font-bold px-5 py-1 text-base rounded-3xl hover:shadow-md outlined-none"
+                >
+                  Save
+                </button>
+              )}
             </div>
-
           </div>
         )}
       </div>
