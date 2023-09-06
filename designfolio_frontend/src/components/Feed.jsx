47a14d067f83b45992255cbd338e48c5a4074bf5
Riya <triya1315@gmail.com>
2023-09-06 22:33:36 +0530

    Format: Feed.jsx

--- designfolio_frontend/src/components/Feed.jsx
+++ designfolio_frontend/src/components/Feed.jsx
@@ -1,46 +1,36 @@
-import React,{useState, useEffect} from 'react'
-import { useParams } from 'react-router-dom'
-import { client } from '../client'
-import MasonryLayout from './MasonryLayout'
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { client } from "../client";
+import MasonryLayout from "./MasonryLayout";
 import Spinner from "./Spinner";
-import { feedQuery, searchQuery } from '../utils/data';
-
+import { feedQuery, searchQuery } from "../utils/data";
 
 const Feed = () => {
   const [loading, setLoading] = useState(false);
   const [pins, setPins] = useState(null);
-  const {categoryId}= useParams();
+  const { categoryId } = useParams();
 
-  useEffect(()=>{
+  useEffect(() => {
     setLoading(true);
-    if(categoryId){
-      const query=searchQuery(categoryId);
-      client.fetch(query)
-      .then((data)=>{
+    if (categoryId) {
+      const query = searchQuery(categoryId);
+      client.fetch(query).then((data) => {
         setPins(data);
         setLoading(false);
-      })
-
-    }else{
-      client.fetch(feedQuery)
-      .then((data)=>{
+      });
+    } else {
+      client.fetch(feedQuery).then((data) => {
         setPins(data);
         setLoading(false);
-      })
-
+      });
     }
+  }, [categoryId]);
 
-  },[categoryId])
+  if (loading)
+    return <Spinner message="We are adding new design to your feed" />;
+  if (!pins?.length) return <h2>able</h2>;
 
-  if(loading) return <Spinner message="We are adding new design to your feed"/>
-  if(!pins?.length) return <h2>No pins available</h2>
- 
+  return <div>{pins && <MasonryLayout pins={pins} />}</div>;
+};
 
-  return (
-    <div>
-      {pins && <MasonryLayout pins={pins}/>}
-    </div>
-  )
-}
-
-export default Feed
+export default Feed;
